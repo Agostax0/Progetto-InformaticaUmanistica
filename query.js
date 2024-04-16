3c83a7799b72a6fb6713704449509b83316c27d4
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2024-04-16 18:33:40 +0200

    example of vertical disposition

--- query.js
+++ query.js
@@ -5,25 +5,10 @@ const historicOrArtisticProperty = "HistoricOrArtisticProperty";
 const culturalProperty = "CulturalProperty";
 const archaeologicalProperty = "ArchaeologicalProperty";
 
-function queryCaricamentoPagina(){
-    var queryURL = 
-    "https://dati.cultura.gov.it/sparql?default-graph-uri=&"
-    + format_json
-    +"debug=on&query=" 
-    +"PREFIX arco-arco: <https://w3id.org/arco/ontology/arco/>"
-    +"PREFIX arco-cd: <https://w3id.org/arco/ontology/context-description/>"
-    +"SELECT DISTINCT *"
-    +" FROM <https://w3id.org/arco/ontology>"
-    +" FROM <https://w3id.org/arco/data>"
-    +" WHERE {"
-    +" ?cultpro rdf:type/rdfs:subClassOf* arco-arco:"+ historicOrArtisticProperty + " ; " 
-    +" rdfs:label ?label ; "
-    +" foaf:depiction ?foto "
-    +" FILTER(lang(?label) = 'it')"
-    +" }"
-    +" LIMIT 10"
-    return queryURL;
-}
+const maxCardsDisplayed = 50;
+
+const halfCardSize = "6";
+const fullCardSize = "12";
 
 function queryConFiltri(parolaChiave, regione){
     var filteredQueryURL = 
@@ -52,7 +37,7 @@ function queryConFiltri(parolaChiave, regione){
                         + " FILTER regex(?regNome, \"" + regione + "\", \"i\") ";
     }
     filteredQueryURL+= "}";
-    filteredQueryURL+="LIMIT 10";
+    filteredQueryURL+="LIMIT " + maxCardsDisplayed;
     console.log(filteredQueryURL);
     return filteredQueryURL;
 }
@@ -84,82 +69,80 @@ function generateCardElement(src,desc,link){
     return card;
 }
 
-function generateTable(data){
-    const tableNode = document.createElement("table");
-
-    tableNode.className = "table table-primary"
-    //generate Table Head
-    const tableHead = document.createElement("thead");
-    var headers = data.head.vars
-    var tableHeadRow = document.createElement("tr");
-    data.head.vars.forEach(element => {
-        //console.log(element);
-        var tableHeadHeader = document.createElement("th");
-        tableHeadHeader.textContent = element;
-        tableHeadRow.appendChild(tableHeadHeader);
-    });
-    tableHead.appendChild(tableHeadRow)
-    tableNode.appendChild(tableHead);
-
-
-
-    //generate Table Body
-    const tableBody = document.createElement("tbody");
-    data.results.bindings.forEach(element=> {
-        var tableBodyRow = document.createElement("tr");
-        headers.forEach(header =>{
-            var tableBodyRowElement = document.createElement("td");
-            console.log(element[header]);
-            //TODO operazioni sul 'type'
-            if(element[header]["type"] == "uri"){
-                
-                if(header == "foto"){
-                    var img = document.createElement("img");
-                    img.src = element[header]['value'];
-                    img.height = 100;
-                    img.width = 100;
-                    tableBodyRowElement.appendChild(img);
-                }
-                else{
-                    var a = document.createElement("a");
-                    a.href = element[header]['value'];
-                    a.text = element[header]['value'];
-                    
-                    tableBodyRowElement.appendChild(a);
-                }
-            }
-            else{
-                tableBodyRowElement.textContent = element[header]['value'];
-            }
-
-
-            tableBodyRow.appendChild(tableBodyRowElement);
-        });
-        tableBody.appendChild(tableBodyRow);
-    });
-    tableNode.appendChild(tableBody);
-    
-    
-    console.log(tableNode);
-    return tableNode;
-}
-
 function generateCards(data){
-    var cardList = document.getElementById("QueryResultDiv");
-    
+    console.log(data.results.bindings.length);
+
+    var row1 = document.getElementById("row1");
+    var row2 = document.getElementById("row2");
+    var row3 = document.getElementById("row3");
+    var row4 = document.getElementById("row4");
+
+    var genericCol = document.createElement("div");
+    genericCol.className = "text-truncate col-";
+
+    //divido i risultati nelle 4 righe
+
+    var selectedRow = 0;
     data.results.bindings.forEach(element=> {
 
-        var colDiv = document.createElement("div");
-        colDiv.className = "col-3";
-
-        colDiv.appendChild(generateCardElement(
+        var card = generateCardElement(
             element['foto']['value'],
             element['label']['value'],
-            element['cultpro']['value']));
+            element['cultpro']['value']);
 
-        cardList.appendChild(colDiv);
+        var cardSize = Math.random() > 0.5;
 
+        if(cardSize){
+            console.log("COLONNA INTERA");
+        }
+
+        //eseguire l'append al padre corretto
+
+        switch(selectedRow){
+            case 0:
+
+                var cardCol = genericCol.cloneNode();
+
+                //aggiungi a row1
+                //controllare che vi siano elementi in row1
+                if(row1.children.length>0){
+                    //verificare la dimensione dell'ultimo elemento 
+                    //ed aggiungere una size dimezzata o intera
+
+                    var isLastCardFullSize = row1.children[row1.children.length-1].className.includes(fullCardSize);
+
+                    
+                    if(isLastCardFullSize){
+                        cardCol.className += (cardSize) ? fullCardSize : halfCardSize;
+                    }
+                    else{
+                        cardCol.className += halfCardSize;
+                    }
+                }
+                else{
+                    //aggiungere semplicemente la size generata
+                    cardCol.className += (cardSize) ? fullCardSize : halfCardSize;
+                }
+
+                cardCol.appendChild(card);
+                row1.appendChild(cardCol);
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+                
+                break;
+            case 3:
+
+                break;
+        }
+
+        selectedRow = (selectedRow + 1) % 4;
     });
+
+
 
 }
 
