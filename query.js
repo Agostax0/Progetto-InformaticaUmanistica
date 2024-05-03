bf07dae8449509aebdf142e279c93ad2b854231f
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2024-05-03 09:42:43 +0200

    added region filters

--- query.js
+++ query.js
@@ -15,7 +15,10 @@ function queryConFiltri(parolaChiave, regione){
     + format_json
     +"debug=on&query=" 
     +"PREFIX arco-arco: <https://w3id.org/arco/ontology/arco/> "
-    +" PREFIX arco-cd: <https://w3id.org/arco/ontology/context-description/> "
+    +"PREFIX arco-cd: <https://w3id.org/arco/ontology/context-description/> "
+    +"PREFIX arco-location: <https://w3id.org/arco/ontology/location/> "
+    +"PREFIX CLV: <https://w3id.org/italia/onto/CLV/> "
+    +"PREFIX arco-dd: <https://w3id.org/arco/ontology/denotative-description/> "
     +" SELECT DISTINCT * "
     +" FROM <https://w3id.org/arco/ontology> "
     +" FROM <https://w3id.org/arco/data> "
@@ -23,7 +26,8 @@ function queryConFiltri(parolaChiave, regione){
     +" ?cultpro rdf:type/rdfs:subClassOf* arco-arco:"+ historicOrArtisticProperty + " ; " 
     +" rdfs:label ?label" + " ; "
     +" arco-cd:hasDocumentation ?documentation" + " ; "
-    +" foaf:depiction ?foto "
+    +" foaf:depiction ?foto" + " ; "
+    +" arco-cd:hasSubject ?sub . ?sub rdfs:label ?subLabel "
     +" FILTER(lang(?label) = 'it') ";
     if(parolaChiave != ""){
         filteredQueryURL+= "FILTER(REGEX(STR(?subLabel), \"" + parolaChiave + "\", \"i\")) ";
@@ -66,7 +70,6 @@ function generateCardElement(src,desc,link){
 }
 
 function generateCards(data){
-    console.log(data.results.bindings.length);
 
     var row1 = document.getElementById("row1");
     var row2 = document.getElementById("row2");
@@ -114,12 +117,60 @@ function generateCards(data){
 
 }
 
-//i parametri da inserire nei filtri vengono presi dai parametri get della pagina index
-axios.get(queryConFiltri("",""))
-.then(function(value){
-    console.log(value);
-    generateCards(value.data);
-})
-.catch(function(error){
-    console.log(error);
+var selectedRegione = "";
+document.getElementById("RegioneCalabria").addEventListener('click', (evt)=>{selectedRegione="Calabria"});
+document.getElementById("RegioneLazio").addEventListener('click', (evt)=>{selectedRegione="Lazio"});
+document.getElementById("RegioneLiguria").addEventListener('click', (evt)=>{selectedRegione="Liguria"});
+document.getElementById("RegioneMarche").addEventListener('click', (evt)=>{selectedRegione="Marche"});
+document.getElementById("RegioneMolise").addEventListener('click', (evt)=>{selectedRegione="Molise"});
+document.getElementById("RegionePuglia").addEventListener('click', (evt)=>{selectedRegione="Puglia"});
+document.getElementById("RegioneSardegna").addEventListener('click', (evt)=>{selectedRegione="Sardegna"});
+document.getElementById("RegioneToscana").addEventListener('click', (evt)=>{selectedRegione="Toscana"});
+document.getElementById("RegioneVeneto").addEventListener('click', (evt)=>{selectedRegione="Veneto"});
+document.getElementById("RegioneEmiliaRomagna").addEventListener('click', (evt)=>{selectedRegione="EmiliaRomagna"});
+document.getElementById("RegioneAbruzzo").addEventListener('click', (evt)=>{selectedRegione="Abruzzo"});
+document.getElementById("RegioneCampania").addEventListener('click', (evt)=>{selectedRegione="Campania"});
+document.getElementById("RegioneLombardia").addEventListener('click', (evt)=>{selectedRegione="Lombardia"});
+document.getElementById("RegionePiemonte").addEventListener('click', (evt)=>{selectedRegione="Piemonte"});
+document.getElementById("RegioneFriuli-VeneziaGiulia").addEventListener('click', (evt)=>{selectedRegione="Friuli-Venezia Giulia"});
+document.getElementById("RegioneUmbria").addEventListener('click', (evt)=>{selectedRegione="Umbria"});
+document.getElementById("RegioneTrentino-AltoAdige").addEventListener('click', (evt)=>{selectedRegione="Trentino-Alto Adige"});
+document.getElementById("RegioneSicilia").addEventListener('click', (evt)=>{selectedRegione="Sicilia"});
+document.getElementById("RegioneBasilicata").addEventListener('click', (evt)=>{selectedRegione="Basilicata"});
+document.getElementById("RegioneValleAosta").addEventListener('click', (evt)=>{selectedRegione="Valle d\'Aosta"});
+
+var searchBar = document.getElementById("searchBar");
+var searchBarText = "";
+searchBar.addEventListener('input', (evt) => {
+    searchBarText = searchBar.value;
 });
+
+var searchBtn = document.getElementById("searchBtn");
+searchBtn.addEventListener('click', (evt) => {
+
+    //svuota i risultati attuali
+
+    document.getElementById("row1").replaceChildren();
+    document.getElementById("row2").replaceChildren();
+    document.getElementById("row3").replaceChildren();
+    document.getElementById("row4").replaceChildren();
+
+    //fai partire la nuova query con tutti i parametri
+
+    //query(searchBarText, selectedRegione);
+});
+
+function query(keyword,region){
+    axios.get(queryConFiltri(keyword,region))
+    .then(function(value){
+        console.log(value);
+        generateCards(value.data);
+    })
+    .catch(function(error){
+        console.log(error);
+    });    
+}
+
+
+
+//query("","");
